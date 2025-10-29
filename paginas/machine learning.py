f50c3c3bbee92c704025b46a23e9933d0a621d52
Cyanah <mirtheblossom2001@hotmail.com>
2025-10-29 11:33:54 +0100

    Create machine learning.py

---
+++ paginas/machine learning.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Titanic Model Accuracy Dashboard", layout="wide")
+
+st.title("🚢 Titanic Model Accuracy Dashboard")
+
+df = 
